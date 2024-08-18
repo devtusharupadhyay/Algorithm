bac4ff25eb54b28c543097f3c02fa0a459741334
Tushar Upadhyay <dev@tusharupadhyay.com>
2024-08-18 10:59:45 +0530

    select all items using LINQ

---
+++ LINQLearn/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using DataLayer.Models;
+
+//Instantiate the ViewModel
+
+SamplesViewModel vm = new SamplesViewModel()
+{
+    UseQuerySyntax = true,  
+};
+
+//call a sample method
+
+vm.GetAll();
+
+//Display Product Collection
+
+foreach (var item in vm.Products) 
+{
+    Console.WriteLine(item.ToString());
+}
+
+// Display Result Text
+Console.WriteLine(vm.ResultText);
